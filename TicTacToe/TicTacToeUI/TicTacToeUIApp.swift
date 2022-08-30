d8df22dea9bc9803c3af4e3991d93e9a592ccbac
Ujjwal R <108294627+iamujjwalrawat@users.noreply.github.com>
2022-08-30 16:47:31 +0530

    Add files via upload

---
+++ TicTacToe/TicTacToeUI/TicTacToeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeUIApp.swift
+//  TicTacToeUI
+//
+//  Created by Ujjwal Rawat on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
